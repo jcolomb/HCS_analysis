61130f1f53815cf0ca6a17002af70a46e04378db
jcolomb <julien.colomb@fu-berlin.de>
2017-09-04 18:58:02 +0200

    update master

--- master_noshiny.R
+++ master_noshiny.R
@@ -3,6 +3,9 @@
 
 library (tidyverse)
 library (stringr)
+#for plotting
+library(gridExtra)
+library(RGraphics)
 #library (tcltk2)
 
 # variables
@@ -19,6 +22,7 @@ Name_project ="Tarabykin" #must be exactly the same in PMeta
 WD = dirname(PMeta)
 Outputs = paste(WD,Projects_metadata$Folder_path,"Routputs", sep="/")
 dir.create (Outputs)
+plot.path = Outputs
 
 ##code
 #read metadata
@@ -28,6 +32,11 @@ source("Rcode/inputdata.r")
 #create list of filepath for each animal_ID
 source("Rcode/checkmetadata.r")
 
-#create and save event files (one file for all mice)
-source ("Rcode/create_eventfile.r")
+#create and save event and minute files (one file for all mice): returns EVENT_data and MIN_data
+# some warnings appear because the last line of the minute data is the sum.
+# this is not relevant because the data is cut at 22.5 hours of data.
 
+source ("Rcode/create_eventfile.r") # to be modified: this takes the whole recording
+source ("Rcode/create_minfile.r")
+
+#event analysis (total time only)
