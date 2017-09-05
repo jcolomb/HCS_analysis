a9b000225c3aad31cf48b3832e5ea6195ebe9b4f
colombj <julien.colomb@fu-berlin.de>
2017-09-05 14:09:45 +0200

    work with the rawdata

---
+++ Rcode/create_rawdatafiles.R
@@ -0,0 +1,46 @@
+#-----------------create rawdata file-----------------#
+
+
+data = data.frame()
+files = as.character(BEH_datafiles[,1])
+
+
+for (f in 1:length(files)) {
+  print(f)
+  behav.raw<-readxl::read_excel (files[f], sheet = 1, skip = 10, col_types =c ("skip","skip", "numeric","text", "skip"))
+  colnames(behav.raw) = c('length_s','Behavior')
+  behav.raw= behav.raw %>% mutate( time_start=cumsum(length_s)-length_s)
+  
+  
+  
+  behav.raw$ID = BEH_datafiles[f,2]
+  
+  #metadata %>% filter (ID == BEH_datafiles[f,2])
+  #df= metadata  %>% filter (ID == BEH_datafiles[f,2]) %>% select(ID, animal_ID = 'animal ID', gender, treatment, genotype, date, test.cage='test cage', 
+  #                                                               real.time = 'real time start', dark.start = light_off, project.name = Proj_name)
+  
+  #df = left_join(behav.1,df)
+  #temp= names(data)
+  data = rbind(data, behav.raw)
+}
+test=data %>% filter (Behavior == "Sleep")
+min(test$length_s)
+test [test$length_s == "0.08",]
+
+
+  write.table(data, paste0(Outputs,'/Events_',Name_project,'.csv'), sep = ';',row.names = FALSE)
+
+#progress = 2
+#setTkProgressBar(pp.pb, progress, label=paste( round(progress/5*100, 0),"% done"))
+mybiglist <- vector('list', length(files))
+names(mybiglist) <- BEH_datafiles[,2]
+for(f in seq_along(mybiglist)){
+  behav.raw<-readxl::read_excel (files[f], sheet = 1, skip = 10, col_types =c ("skip","skip", "numeric","text", "skip"))
+  colnames(behav.raw) = c('length_s','Behavior')
+  behav.raw= behav.raw %>% mutate( time_start=cumsum(length_s)-length_s)
+  
+  
+  
+  mybiglist[[f]] <- list(rawdata=behav.raw)
+  
+}
