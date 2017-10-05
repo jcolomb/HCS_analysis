4102f707c6d19a2ef3637d96a33549cb0e57f48c
jcolomb <julien.colomb@fu-berlin.de>
2017-10-05 17:01:59 +0200

    working with more than 2 groups

---
+++ analysis/Rcode/Tuning_svm2.R
@@ -0,0 +1,56 @@
+## This code perform a svm on the subset of data and the whole data and choose the model fitting the traindataset at best.
+
+##Subset 0f variables:
+
+##data input
+trainset2 =trainset [,names(trainset) %in% c(as.character(R2 [1:numberofvariables,1]), "groupingvar") ]
+testset2 =testset [,names(testset) %in% c(as.character(R2 [1:numberofvariables,1]), "groupingvar") ]
+
+##tuning and performing svm  on trainset
+bestk=NA
+bestk= tune.svm2(trainset2,groupingvar)
+
+best.parameters = bestk[[2]]
+
+svm.model <- svm(groupingvar ~ ., data = trainset2, cost = best.parameters$cost, gamma = best.parameters$gamma, kernel = bestk[[1]])
+svm.pred <- predict(svm.model, trainset2 %>% select(-groupingvar))
+
+SVMprediction_res =table(pred = svm.pred, true = trainset2$groupingvar)
+
+
+#Accuracy of grouping and save model
+temp =classAgreement (SVMprediction_res)
+subsetcrand=temp$crand
+subsetmodel=svm.model
+
+##ALL variables:
+###
+
+##tuning and performing svm  
+bestk=NA
+bestk= tune.svm2(trainset,groupingvar)
+best.parameters = bestk[[2]]
+
+
+svm.model <- svm(groupingvar ~ ., data = trainset, cost = best.parameters$cost, gamma = best.parameters$gamma, kernel = bestk[[1]])
+
+svm.pred <- predict(svm.model, trainset %>% select(-groupingvar))
+
+SVMprediction_res =table(pred = svm.pred, true = trainset$groupingvar)
+
+
+#Accuracy of grouping and plot
+temp =classAgreement (SVMprediction_res)
+allcrand=temp$crand
+allmodel=svm.model
+
+if (allcrand<  subsetcrand) {
+  # get subset model insteadSVM also train and test sets
+  svm.model = subsetmodel
+  Input  =Multi_datainput_m [,names(Multi_datainput_m) %in% c(as.character(R2 [1:numberofvariables,1]), "groupingvar") ]
+  Glass= Input %>% filter (groupingvar == L[1])
+  Glass2= Input %>% filter (groupingvar == L[2])
+  testset   <- rbind(Glass[testindex,],Glass2[testindex,])
+  trainset  <- rbind(Glass[-testindex,],Glass2[-testindex,])
+}
+
--- analysis/Rcode/morethan2groups.r
+++ analysis/Rcode/morethan2groups.r
@@ -1,11 +1,35 @@
-if (length(unique(metadata$groupingvar))>2){
-  print(we need to choose grouping variables)
+if (length(unique(metadata$groupingvar))==3) {
+  metadata_ori= metadata
+  Multi_datainput_m_ori = Multi_datainput_m
+  
+  print("we need to choose grouping variables")
   metadata$groupingvar = as.factor(metadata$groupingvar)
-
-  metadataRest= metadata [metadata$groupingvar %in% levels(metadata$groupingvar)[1:2],]
-  metadataRest=droplevels(metadataRest)
+  
+  
+  metadataRest= metadata_ori [metadata_ori$groupingvar %in% levels(metadata_ori$groupingvar)[1:2],]
+  metadata=droplevels(metadataRest)
+  Multi_datainput_mREST = Multi_datainput_m_ori [metadata_ori$groupingvar %in% levels(metadata_ori$groupingvar)[1:2],]
+  Multi_datainput_m= droplevels(Multi_datainput_mREST)
   # make svm!
+  source ("Rcode/multidimensional_analysis_runnostat.R")
+  p1=print(paste0 ("For the groups ", paste (unique(metadata$groupingvar), collapse= " "),": ", Accuracy))
   
   
+  metadataRest= metadata_ori [metadata_ori$groupingvar %in% levels(metadata_ori$groupingvar)[2:3],]
+  metadata=droplevels(metadataRest)
+  Multi_datainput_mREST = Multi_datainput_m_ori [metadata_ori$groupingvar %in% levels(metadata_ori$groupingvar)[2:3],]
+  Multi_datainput_m= droplevels(Multi_datainput_mREST)
+  # make svm!
+  source ("Rcode/multidimensional_analysis_runnostat.R")
+  p2=print(paste0 ("For the groups ", paste (unique(metadata$groupingvar), collapse= " "),": ", Accuracy))
   
-  }
+  metadataRest= metadata_ori [metadata_ori$groupingvar %in% levels(metadata_ori$groupingvar)[c(1,3)],]
+  metadata=droplevels(metadataRest)
+  Multi_datainput_mREST = Multi_datainput_m_ori [metadata_ori$groupingvar %in% levels(metadata_ori$groupingvar)[c(1,3)],]
+  Multi_datainput_m= droplevels(Multi_datainput_mREST)
+  # make svm!
+  source ("Rcode/multidimensional_analysis_runnostat.R")
+  p3=print(paste0 ("For the groups ", paste (unique(metadata$groupingvar), collapse= " "),": ", Accuracy))
+   Multi_datainput_m =Multi_datainput_m_ori 
+   metadata =metadata_ori
+}
