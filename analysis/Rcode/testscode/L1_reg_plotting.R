a59e5d6f31cacd389efdc2e86a4365fdd95c0ff7
jcolomb <julien.colomb@fu-berlin.de>
2017-11-03 16:44:55 +0100

    test plotting

--- analysis/Rcode/testscode/L1_reg_plotting.R
+++ analysis/Rcode/testscode/L1_reg_plotting.R
@@ -8,24 +8,24 @@ RF_selec = RF_selec[order(RF_selec$groupingvar),]
 
 xx= as.matrix(RF_selec %>% select (-groupingvar))
 yy = as.numeric(RF_selec$groupingvar)-1
-#pp  <- rep(NA, length(yy))
+pp  <- rep(NA, length(yy))
 
-#num_per_class <- nrow(RF_selec)/2
+num_per_class <- nrow(RF_selec)/2
 
-# for (k in 1:num_per_class) {
-#   hold_out = c(k, k+num_per_class) ## hold out one pos and one neg examples so samples are still balanced 
-#   fit <- glmpath(x=xx[-hold_out,], y=yy[-hold_out], family=binomial, max.arclength=1)
-#   bestdex <- which.min(fit$bic)
-#   if (bestdex==1) {bestdex <- 2}  ## break ties by avoiding degenerate classifiers 
-#   bestlambda <- fit$lambda[bestdex]
-#   pred <- predict(fit, newx=xx[hold_out,], s=bestlambda, type="link", mode="lambda") 
-#   pp[hold_out] = pred
-# }
-# 
-# true_class <- sign(yy - 0.5)
-# pred_class <- sign(pp); 
-# 
-# print(table(true_class, pred_class))
+for (k in 1:num_per_class) {
+  hold_out = c(k, k+num_per_class) ## hold out one pos and one neg examples so samples are still balanced
+  fit <- glmpath(x=xx[-hold_out,], y=yy[-hold_out], family=binomial, max.arclength=1)
+  bestdex <- which.min(fit$bic)
+  if (bestdex==1) {bestdex <- 2}  ## break ties by avoiding degenerate classifiers
+  bestlambda <- fit$lambda[bestdex]
+  pred <- predict(fit, newx=xx[hold_out,], s=bestlambda, type="link", mode="lambda")
+  pp[hold_out] = pred
+}
+
+true_class <- sign(yy - 0.5)
+pred_class <- sign(pp);
+
+print(table(true_class, pred_class))
 
 
 fit <- glmpath(x=xx, y=yy, family=binomial, max.arclength=1)
@@ -54,6 +54,11 @@ RF_selec%>% ggplot(aes (y=Index, x=groupingvar))+
 
 
 
+
+library(plotly)
+plot_ly(x=Input$Eat1  ,y=5*Input$digforage3 ,z=Input$RemainLow4 , color=Input$groupingvar, type="scatter3d")
+
+plot_ly(y=Input$walk2 ,x=Input$ComeDown5 , color=Input$groupingvar, type="scatter")
 
 
 
