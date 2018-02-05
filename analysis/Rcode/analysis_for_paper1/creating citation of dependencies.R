ec8f88898ca8de5b7b973bc07b7284a72a601c7a
jcolomb <julien.colomb@fu-berlin.de>
2018-02-05 17:17:16 +0100

    change to project metadata to get better visualisation on paper

--- analysis/Rcode/analysis_for_paper1/creating citation of dependencies.R
+++ analysis/Rcode/analysis_for_paper1/creating citation of dependencies.R
@@ -1,3 +1,19 @@
 sink("test.bib")
 out <- sapply(names(sessionInfo()$otherPkgs), 
               function(x) print(citation(x), style = "Bibtex"))
+
+
+sink("analysis/materialforpaper/projectmetadata.tex")
+temp=t(Projects_metadata)
+temp=as.data.frame(temp)
+Pmeta2=xtable::xtable(temp,
+                      align = "|p{0.18\textwidth}|p{0.27\textwidth}|",
+                      caption = "Master metadata information.",
+                      label = "tab:project_metadata")
+print (Pmeta2, 
+       table.placement= "!htpb",
+       include.colnames =F#,
+       #tabular.environment ="tabular*",
+       #width ="\\linewidth"
+       )
+sink()
